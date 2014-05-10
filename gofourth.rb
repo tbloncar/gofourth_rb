e6db1d1c690c42285dae0eac72490b59074baea9
Travis Loncar <loncar.travis@gmail.com>
2014-05-10 09:37:06 -0400

    Add rocket sprites and highlight quads.

--- gofourth.rb
+++ gofourth.rb
@@ -1,65 +1,42 @@
 require "gosu"
-
-LEVELS = [
-  {
-    problem: "sqrt(5! + 1)",
-    possible_answers: [121, 6, 9, 11],
-    solution_quad: 4
-  },
-  {
-    problem: "2 * 2 + 3 * 4 - 5",
-    possible_answers: [35, 11, -12, 23],
-    solution_quad: 2
-  },
-  {
-    problem: "3^2 * 2 - 4",
-    possible_answers: [14, 3, 77, -18],
-    solution_quad: 1
-  },
-   {
-    problem: "1^32 - 0!",
-    possible_answers: [1, 0, -1, 32],
-    solution_quad: 2
-  },
-   {
-    problem: "5! / 2! + 3!",
-    possible_answers: [72, 3.5, 66, 99],
-    solution_quad: 3
-  },
- 
-]
+require "pstore"
+require_relative "levels"
+require_relative "rocket"
 
 class Game < Gosu::Window
-  attr_reader :font
+  attr_reader :font, :header_height, :quad_width, :quad_height
 
   def initialize
     super(500, 350, false)
     self.caption = "GoFourth"
 
-    @piece_width    = 20
     @header_height  = 60
     @quad_width     = self.width/2
     @quad_height    = (self.height - @header_height)/2
-    @move_distance  = 10
+    @piece          = Rocket.new(self)
     @background     = Gosu::Image.new(self, "media/img/background.jpg", true)
     @font           = "media/font/Abel-Regular.ttf"
     @stage          = :start
+    @store          = PStore.new("scores.pstore") || 0
     @timer          = 0
     @level          = 0
     @score          = 0
     @levels         = LEVELS.shuffle
-    @x, @y          = self.width/2 - @piece_width/2, self.height/2 - @piece_width/2 + @header_height/2
     @colors         = {
-      gold: Gosu::Color.rgb(255, 185, 15),
+      gold: Gosu::Color.rgba(255, 185, 15, 100),
       faded_gold: Gosu::Color.rgba(255, 185, 15, 30),
-      purple: Gosu::Color.rgb(125, 38, 205),
+      purple: Gosu::Color.rgba(125, 38, 205, 100),
       faded_purple: Gosu::Color.rgba(125, 38, 205, 30),
-      green: Gosu::Color.rgb(127, 255, 0),
+      green: Gosu::Color.rgba(127, 255, 0, 100),
       faded_green: Gosu::Color.rgba(127, 255, 0, 30),
-      red: Gosu::Color.rgb(205, 0, 0),
+      red: Gosu::Color.rgba(205, 0, 0, 100),
       faded_red: Gosu::Color.rgba(205, 0, 0, 30),
+      danger_red: Gosu::Color.rgb(205, 0, 0),
       white: Gosu::Color.rgb(255,255,255)
     }
+    @store.transaction(true) do
+      @best_score = @store[:best_score] || 0
+    end
   end
 
   def button_down(id)
@@ -71,18 +48,17 @@ class Game < Gosu::Window
     when :start
       @stage = :play if button_down?(Gosu::KbReturn)
     when :play
-      respond_to_piece_movement
-      @timer = 500 if button_down?(Gosu::KbReturn) && @timer > 10
+      @piece.update
+      @stage = :answer if button_down?(Gosu::KbReturn) && @timer > 10
+      
     when :answer
       
     when :over
-      if button_down?(Gosu::KbR)
-        @levels = LEVELS.shuffle
-        @level  = 0
-        @score  = 0
-        @timer  = 0
-        @stage  = :play
-      end
+      possibly_set_best_score
+      restart if button_down?(Gosu::KbR)
+    when :win
+      possibly_set_best_score
+      restart if button_down?(Gosu::KbR)
     end
   end
 
@@ -102,14 +78,14 @@ class Game < Gosu::Window
 
       draw_problem(@current_level[:problem])
       draw_possible_answers(@current_level[:possible_answers])
-      draw_piece
       draw_timer
+      @piece.draw
 
       @stage = :answer if @timer == 500
     when :answer
       if check_answer(@current_level[:solution_quad])
         @level += 1
-        @score += 1
+        @score += (500 - @timer) * @level
         @timer = 0
         if @levels[@level]
           @stage = :play
@@ -120,17 +96,9 @@ class Game < Gosu::Window
         @stage = :over
       end
     when :over
-      game_over_text = image_from_text("Game Over!", 60)
-      game_over_text.draw(self.width/2 - game_over_text.width/2, self.height/2 - game_over_text.height/2 - 50, 0)
-      score_text = image_from_text("Score: #{@score}", 30)
-      score_text.draw(self.width/2 - score_text.width/2, self.height/2 - game_over_text.height/2 + 10, 0)
-      action_text  = image_from_text("Press R to Try Again", 36)
-      action_text.draw(self.width/2 - action_text.width/2, self.height/2 - game_over_text.height/2 + 60, 0)
+      draw_over_stage
     when :win
-      win_text = image_from_text("You Win!", 60)
-      win_text.draw(self.width/2 - win_text.width/2, self.height/2 - win_text.height/2 - 50, 0)
-      score_text = image_from_text("Score: #{@score}", 30)
-      score_text.draw(self.width/2 - score_text.width/2, self.height/2 - win_text.height/2 + 10, 0)
+      draw_win_stage
     end
   end
 
@@ -140,39 +108,30 @@ class Game < Gosu::Window
     Gosu::Image.from_text(self, text, font, font_size)
   end
 
-  def respond_to_piece_movement
-    d = @piece_width + @move_distance
-    if button_down?(Gosu::KbRight)
-      @x += (@x > self.width - d ? self.width - @piece_width - @x : @move_distance)
-    end
-    if button_down?(Gosu::KbLeft)
-      @x -= (@x < @move_distance ? @x : @move_distance)
-    end
-    if button_down?(Gosu::KbDown)
-      @y += (@y > self.height - d ? self.height - @piece_width - @y : @move_distance)
-    end
-    if button_down?(Gosu::KbUp)
-      @y -= (@y < @move_distance + @header_height ? @y - @header_height : @move_distance)
-    end
-  end
-
   def draw_quads
-    draw_quad(0, @header_height, @colors[:faded_gold],
-              @quad_width, @header_height, @colors[:faded_gold],
-              @quad_width, @quad_height + @header_height, @colors[:faded_gold],
-              0, @quad_height + @header_height, @colors[:faded_gold])
-    draw_quad(@quad_width, @header_height, @colors[:faded_purple],
-              self.width, @header_height, @colors[:faded_purple],
-              self.width, @quad_height + @header_height, @colors[:faded_purple],
-              @quad_width, @quad_height + @header_height, @colors[:faded_purple])
-    draw_quad(0, @header_height + @quad_height, @colors[:faded_green],
-              @quad_width, @header_height + @quad_height, @colors[:faded_green],
-              @quad_width, self.height, @colors[:green],
-              0, self.height, @colors[:faded_green])
-    draw_quad(@quad_width, @header_height + @quad_height, @colors[:faded_red],
-              self.width, @header_height + @quad_height, @colors[:faded_red],
-              self.width, self.height, @colors[:faded_red],
-              @quad_width, self.height, @colors[:faded_red])
+    quad_1_color = @piece.in_quad_1? ? @colors[:gold] : @colors[:faded_gold]
+    draw_quad(0, @header_height, quad_1_color,
+              @quad_width, @header_height, quad_1_color,
+              @quad_width, @quad_height + @header_height, quad_1_color,
+              0, @quad_height + @header_height, quad_1_color)
+
+    quad_2_color = @piece.in_quad_2? ? @colors[:purple] : @colors[:faded_purple]
+    draw_quad(@quad_width, @header_height, quad_2_color,
+              self.width, @header_height, quad_2_color,
+              self.width, @quad_height + @header_height, quad_2_color,
+              @quad_width, @quad_height + @header_height, quad_2_color)
+
+    quad_3_color = @piece.in_quad_3? ? @colors[:green] : @colors[:faded_green]
+    draw_quad(0, @header_height + @quad_height, quad_3_color,
+              @quad_width, @header_height + @quad_height, quad_3_color,
+              @quad_width, self.height, quad_3_color,
+              0, self.height, quad_3_color)
+
+    quad_4_color = @piece.in_quad_4? ? @colors[:red] : @colors[:faded_red]
+    draw_quad(@quad_width, @header_height + @quad_height, quad_4_color,
+              self.width, @header_height + @quad_height, quad_4_color,
+              self.width, self.height, quad_4_color,
+              @quad_width, self.height, quad_4_color)
   end
 
   def draw_problem(problem)
@@ -214,7 +173,7 @@ class Game < Gosu::Window
     timer_font_size = 40
     timer_image = image_from_text("#{500 - @timer}", timer_font_size)
     timer_image.draw(440, 5, 0) if @timer < 400
-    timer_image.draw(440, 5, 0, 1, 1, @colors[:red]) if @timer >= 400
+    timer_image.draw(440, 5, 0, 1, 1, @colors[:danger_red]) if @timer >= 400
   end
 
   def get_piece_color
@@ -226,23 +185,42 @@ class Game < Gosu::Window
   end
 
   def check_answer(fourth_with_correct_answer)
-    send("in_quad_#{fourth_with_correct_answer}?")
+    @piece.send("in_quad_#{fourth_with_correct_answer}?")
   end
 
-  def in_quad_1?
-    @y < (@quad_height + @header_height - @piece_width/2) && @x < (@quad_width - @piece_width/2)
+  def restart
+    @levels = LEVELS.shuffle
+    @level  = 0
+    @score  = 0
+    @timer  = 0
+    @stage  = :play
   end
 
-  def in_quad_2?
-    @y < (@quad_height + @header_height - @piece_width/2) && @x >= @quad_width
+  def possibly_set_best_score
+    if @score > @best_score
+      @best_score = @score
+      @store.transaction do
+        @store[:best_score] = @best_score
+      end
+    end
   end
 
-  def in_quad_3?
-    @y >= (@quad_height + @header_height) && @x < (@quad_width - @piece_width/2)
+  def draw_over_stage
+    game_over_text = image_from_text("Game Over!", 60)
+    game_over_text.draw(self.width/2 - game_over_text.width/2, self.height/2 - game_over_text.height/2 - 50, 0)
+    score_text = image_from_text("Score: #{@score}  Best: #{@best_score}", 30)
+    score_text.draw(self.width/2 - score_text.width/2, self.height/2 - game_over_text.height/2 + 10, 0)
+    action_text  = image_from_text("Press R to Try Again", 36)
+    action_text.draw(self.width/2 - action_text.width/2, self.height/2 - game_over_text.height/2 + 60, 0)
   end
 
-  def in_quad_4?
-    @y >= (@quad_height + @header_height) && @x >= @quad_width
+  def draw_win_stage
+    win_text = image_from_text("You Win!", 60)
+    win_text.draw(self.width/2 - win_text.width/2, self.height/2 - win_text.height/2 - 50, 0)
+    score_text = image_from_text("Score: #{@score}  Best: #{@best_score}", 30)
+    score_text.draw(self.width/2 - score_text.width/2, self.height/2 - win_text.height/2 + 10, 0)
+    action_text  = image_from_text("Press R to Play Again", 36)
+    action_text.draw(self.width/2 - action_text.width/2, self.height/2 - score_text.height/2 + 40, 0)
   end
 end
 
---
+++ rocket.rb
@@ -0,0 +1,76 @@
+class Rocket
+  attr_reader :width, :height
+
+  def initialize(window)
+    @window         = window
+    @width          = 49
+    @height         = 49
+    @idle           = Gosu::Image.load_tiles(@window,
+                                             "media/img/rocket-sprites.png",
+                                             @width, @height, true)
+    @move_distance  = 10 
+    @direction      = :up
+    @x, @y          = @window.width/2 - @width/2, @window.height/2 - @height/2 + @window.header_height/2
+  end
+
+  def update
+    respond_to_movement
+  end
+
+  def draw
+    set_image 
+    @image.draw(@x, @y, 1, 1, 1)
+  end
+
+  def in_quad_1?
+    @y < (@window.quad_height + @window.header_height - @width/2) && 
+      @x < (@window.quad_width - @width/2)
+  end
+
+  def in_quad_2?
+    @y < (@window.quad_height + @window.header_height - @width/2) &&
+      @x >= @window.quad_width
+  end
+
+  def in_quad_3?
+    @y >= (@window.quad_height + @window.header_height) &&
+      @x < (@window.quad_width - @width/2)
+  end
+
+  def in_quad_4?
+    @y >= (@window.quad_height + @window.header_height) &&
+      @x >= @window.quad_width
+  end
+
+  private
+
+  def respond_to_movement
+    wd = @width + @move_distance
+    hd = @height + @move_distance
+    if @window.button_down?(Gosu::KbRight)
+      @direction = :right
+      @x += (@x > @window.width - wd ? @window.width - @width - @x : @move_distance)
+    end
+    if @window.button_down?(Gosu::KbLeft)
+      @direction = :left
+      @x -= (@x < @move_distance ? @x : @move_distance)
+    end
+    if @window.button_down?(Gosu::KbDown)
+      @direction = :down
+      @y += (@y > @window.height - hd ? @window.height - @width - @y : @move_distance)
+    end
+    if @window.button_down?(Gosu::KbUp)
+      @direction = :up
+      @y -= (@y < @move_distance + @window.header_height ? @y - @window.header_height : @move_distance)
+    end
+  end
+
+  def set_image
+    @image = case @direction
+    when :up then @idle[0]
+    when :down then @idle[1]
+    when :right then @idle[2]
+    when :left then @idle[3]
+    end
+  end
+end
